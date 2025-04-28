228d2916a6a228e574da1a5b0b90b04473df55cf
Andreas <andreas.brandenborg@live.dk>
2025-04-28 11:12:20 +0200

    Added Device class

---
+++ app/src/main/java/com/example/applikationsudvikling_minstroem/data/models/Device.kt
@@ -0,0 +1,9 @@
+package com.example.applikationsudvikling_minstroem.data.models
+
+class Device(name: String, powerUsage: Int) {
+    var Charge: Boolean = false
+
+    fun chooseCharge() {
+        Charge!=Charge
+    }
+}
--- app/src/main/java/com/example/applikationsudvikling_minstroem/viewmodels/Viewmodel.kt
+++ app/src/main/java/com/example/applikationsudvikling_minstroem/viewmodels/Viewmodel.kt
@@ -1,7 +1,12 @@
 package com.example.applikationsudvikling_minstroem.viewmodels
 
+import android.bluetooth.BluetoothClass
 import androidx.lifecycle.ViewModel
+import com.example.applikationsudvikling_minstroem.data.models.Device
 
 class Viewmodel: ViewModel() {
 
+    fun addNewDevice() {
+
+    }
 }
