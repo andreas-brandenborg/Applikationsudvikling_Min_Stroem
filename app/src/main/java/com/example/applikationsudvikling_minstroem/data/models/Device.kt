4e65f925f09a979795151ce42ce828595a8b5bae
Andreas <andreas.brandenborg@live.dk>
2025-04-28 11:40:05 +0200

    Added function to add new devices

--- app/src/main/java/com/example/applikationsudvikling_minstroem/data/models/Device.kt
+++ app/src/main/java/com/example/applikationsudvikling_minstroem/data/models/Device.kt
@@ -1,9 +1,17 @@
 package com.example.applikationsudvikling_minstroem.data.models
 
-class Device(name: String, powerUsage: Int) {
+class Device(id: Int, name: String, powerUsage: Int) {
     var Charge: Boolean = false
 
     fun chooseCharge() {
         Charge!=Charge
     }
 }
+
+val deviceList = mutableListOf<Device>(
+    Device(1, "Ovn", 500),
+    Device(2, "Tørertumbler", 500),
+    Device(3, "Vaskemaskine", 500),
+    Device(4, "Opvasker", 500),
+    Device(5, "El-cykel", 500)
+)
--- app/src/main/java/com/example/applikationsudvikling_minstroem/viewmodels/Viewmodel.kt
+++ app/src/main/java/com/example/applikationsudvikling_minstroem/viewmodels/Viewmodel.kt
@@ -1,12 +1,15 @@
 package com.example.applikationsudvikling_minstroem.viewmodels
 
 import android.bluetooth.BluetoothClass
+import android.icu.lang.UScript.ScriptUsage
 import androidx.lifecycle.ViewModel
 import com.example.applikationsudvikling_minstroem.data.models.Device
+import com.example.applikationsudvikling_minstroem.data.models.deviceList
 
 class Viewmodel: ViewModel() {
 
-    fun addNewDevice() {
-
+    fun addNewDevice(id: Int, name: String, powerUsage: Int) {
+        val newDevice = Device(id, name, powerUsage)
+        deviceList.add(newDevice)
     }
 }
