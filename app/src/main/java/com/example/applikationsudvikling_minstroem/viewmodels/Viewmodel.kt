a99c14b2be0e31b481e7f827e5831c3684a1b123
Andreas <andreas.brandenborg@live.dk>
2025-04-29 09:23:12 +0200

    Added autoincrement function to devices and testet addnewdevice function

--- app/src/main/java/com/example/applikationsudvikling_minstroem/viewmodels/Viewmodel.kt
+++ app/src/main/java/com/example/applikationsudvikling_minstroem/viewmodels/Viewmodel.kt
@@ -1,15 +1,17 @@
 package com.example.applikationsudvikling_minstroem.viewmodels
 
-import android.bluetooth.BluetoothClass
 import android.icu.lang.UScript.ScriptUsage
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.applikationsudvikling_minstroem.data.models.Device
 import com.example.applikationsudvikling_minstroem.data.models.deviceList
 
 class Viewmodel: ViewModel() {
 
-    fun addNewDevice(id: Int, name: String, powerUsage: Int) {
-        val newDevice = Device(id, name, powerUsage)
+    fun addNewDevice(name: String, powerUsage: Int) {
+        val autoincrementId = deviceList.size + 1
+        val newDevice = Device(autoincrementId, name, powerUsage)
         deviceList.add(newDevice)
+        println(deviceList)
     }
 }
