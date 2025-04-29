e82bd3c297addda6a86d2b6299a73e24695d3f2f
vilo0001 <vilo0001@stud.kea.dk>
2025-04-29 09:17:13 +0200

    Top padding og tekst til graf.

--- app/src/main/java/com/example/applikationsudvikling_minstroem/ui/screens/Main_screen.kt
+++ app/src/main/java/com/example/applikationsudvikling_minstroem/ui/screens/Main_screen.kt
@@ -13,7 +13,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -27,12 +29,14 @@ import com.example.applikationsudvikling_minstroem.R
 import com.example.applikationsudvikling_minstroem.navigation.Routes
 
 val dates = listOf("I dag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag", "Søndag")
+val currentDate = "29/04/2025"
 
 @Composable
 fun Main_screen(navController: NavController) {
     Column(
         modifier = Modifier
             .padding(24.dp)
+            .padding(top = 15.dp)
     ) {
         GraphSection(dates)
         Button(onClick = {
@@ -65,11 +69,14 @@ fun GraphSection(days: List<String>) {
                     fontSize = 16.sp)
             }
         }
-        Image(
-            painter = painterResource(id = R.drawable.graph),
-            contentDescription = "Graph of electricity prices",
-            contentScale = ContentScale.FillWidth,
-            modifier = Modifier.fillMaxWidth()
-        )
+        Column {
+            Text("Elpriser $")
+            Image(
+                painter = painterResource(id = R.drawable.graph),
+                contentDescription = "Graph of electricity prices",
+                contentScale = ContentScale.FillWidth,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
     }
 }
