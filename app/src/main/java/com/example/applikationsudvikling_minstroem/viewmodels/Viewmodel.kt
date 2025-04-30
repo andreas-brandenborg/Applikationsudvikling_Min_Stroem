a269518cce14c69612d502ad447c150cc4c202be
Andreas <andreas.brandenborg@live.dk>
2025-04-30 10:33:59 +0200

    Added functionality to recieve best price called getBestPrice and removed MinStroemViewModel.kt

--- app/src/main/java/com/example/applikationsudvikling_minstroem/viewmodels/Viewmodel.kt
+++ app/src/main/java/com/example/applikationsudvikling_minstroem/viewmodels/Viewmodel.kt
@@ -3,13 +3,27 @@ package com.example.applikationsudvikling_minstroem.viewmodels
 import android.bluetooth.BluetoothClass
 import android.icu.lang.UScript.ScriptUsage
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.applikationsudvikling_minstroem.data.models.Device
 import com.example.applikationsudvikling_minstroem.data.models.deviceList
+import com.example.applikationsudvikling_minstroem.network.RetrofitInstance
+import kotlinx.coroutines.runBlocking
 
 class Viewmodel: ViewModel() {
 
     fun addNewDevice(id: Int, name: String, powerUsage: Int) {
         val newDevice = Device(id, name, powerUsage)
         deviceList.add(newDevice)
+    }
+
+    fun getBestPrice() {
+        val token = System.getenv("Token") // Retrieve the token from environment variables
+        runBlocking {
+            val instance = RetrofitInstance()
+            val prices = instance.apiService.getPrices("Bearer $token")
+
+            val minPrices = prices.minByOrNull { it.price }
+            println(minPrices)
+        }
     }
 }
